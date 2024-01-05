718943101cf0136f976ca442af36b1dae5a9ced3
tomas-barros1 <tomasabbarros3@gmail.com>
2024-01-04 23:49:59 -0300

    third commit

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const id = document.getElementById("id")
 const button = document.getElementById("button")
 const name = document.getElementById("name")
 const image = document.getElementById("image")
@@ -12,13 +11,13 @@ function usingApi(id) {
 }
 
 button.addEventListener("click", async () => {
-  const response = await usingApi(id.value)
+  const response = await usingApi(Math.floor(Math.random() * 827))
   if(response.name != null) {
     name.innerHTML = response.name
     image.setAttribute("src", response.image)
   }
   else {
     name.innerHTML = "Não há personagem com esse ID!"
+    image.setAttribute("src", "")
   }
 })
-
