aa705983644fa02ac39ffaecf24b70001f515c75
tomas-barros1 <tomasabbarros3@gmail.com>
2024-04-28 14:25:50 -0300

    code improvements

--- script.js
+++ script.js
@@ -2,22 +2,20 @@ const button = document.getElementById("button")
 const name = document.getElementById("name")
 const image = document.getElementById("image")
 
-function usingApi(id) {
-  const response = fetch(`https://rickandmortyapi.com/api/character/${id}`)
-  .then((resp) => resp.json())
-  .then((data) => (data))
-
-  return response
+async function fetchData(id) {
+  const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
+  const data = await response.json(); 
+  return data;
 }
 
 button.addEventListener("click", async () => {
-  const response = await usingApi(Math.floor(Math.random() * 827))
-  if(response.name != null) {
-    name.innerHTML = response.name
-    image.setAttribute("src", response.image)
+  const response = await fetchData(Math.floor(Math.random() * 827))
+  if (response.name != null) {
+    name.innerHTML = response.name;
+    image.setAttribute("src", response.image);
   }
   else {
-    name.innerHTML = "Não há personagem com esse ID!"
-    image.setAttribute("src", "")
+    name.innerHTML = "Não há personagem com esse ID!";
+    image.setAttribute("src", "");
   }
 })
